cf71914017cb90306106dbb7c4bf37315e982cc9
kaitlyngoh <e0774226@u.nus.edu>
2023-03-25 22:51:41 +0800

    basic calendar template

---
+++ src/components/FullCalendar.vue
@@ -0,0 +1,33 @@
+<script>
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
+export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data: function() {
+    return {
+      calendarOptions: {
+        plugins: [dayGridPlugin],
+        initialView: 'dayGridMonth',
+        weekends: true,
+        // events: [
+        //   { title: 'Meeting', start: new Date() }
+        // ]
+      }
+    }
+  },
+  mounted() {
+    this.calendar = new Calendar(this.$el, this.calendarOptions);
+    this.calendar.render();
+  },
+  beforeDestroy() {
+    this.calendar.destroy();
+  },
+}
+</script>
+
+<template>
+  <FullCalendar :options='calendarOptions' />
+</template>
--- src/views/CalendarPage.vue
+++ src/views/CalendarPage.vue
@@ -1,17 +1,44 @@
 <template> 
-<div>
-    <NavigationBar/>
-    <h1>THIS IS CALENDAR PAGE PAGE</h1>
-</div>
-</template>
+    <div>
+      <NavigationBar/>
+      <div class="calendar-wrapper">
+        <FullCalendar class="calendar-container" />
+      </div>
+      <!-- <full-calendar :plugins="calendarPlugins" :initialView="'dayGridMonth'"/> -->
+    </div>
+  </template>
+    
+  <script>
+  import NavigationBar from "@/components/NavigationBar.vue";
+  import FullCalendar from "@/components/FullCalendar.vue";
+//   import dayGridPlugin from "@fullcalendar/daygrid";
+//   import "@fullcalendar/common/main.css";
+//   import "@fullcalendar/daygrid/main.css";
   
-<script>
-import NavigationBar from "@/components/NavigationBar.vue"
-  
-export default {
-    name:"CalendarPage" ,
-    components : {
-        NavigationBar,
+  export default {
+    name: "CalendarPage",
+    components: {
+      NavigationBar,
+      FullCalendar,
     },
+    data() {
+      return {
+        // calendarPlugins: [dayGridPlugin],
+      };
+    },
+  };
+  </script>
+  
+<style>
+.calendar-wrapper {
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  width: 100%;
 }
-</script>
+
+.calendar-container {
+  width: 60%;
+  height: auto;
+}
+</style>
