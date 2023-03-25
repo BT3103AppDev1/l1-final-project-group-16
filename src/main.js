c743cddecd374a07226bcc32919efc6d7b5d5bfc
lu-yi-fan <88626655+Lu-Yi-Fan@users.noreply.github.com>
2023-03-25 14:46:23 +0800

    override bootstrap colour
    -

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import App from './App.vue'
 import router from './router/index.js'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './custom.css';
 
 createApp(App).use(router).mount('#app')
