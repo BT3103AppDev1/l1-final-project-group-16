d0ce012ef5199fdebb7ec75d5d16eab318e05ed5
kaitlyngoh <e0774226@u.nus.edu>
2023-03-26 03:06:39 +0800

    can press to get date

--- src/components/FullCalendar.vue
+++ src/components/FullCalendar.vue
@@ -1,10 +1,11 @@
 <template>
-  <FullCalendar 
-    ref="calendar"
-    :options="calendarOptions"
-    @dateClick="handleDateClick"/>
+  <div class="calendarComponent">
+    <FullCalendar 
+      ref="calendar"
+      :options="calendarOptions"/><br>
+      <button v-on:click="logSelectedDate">Enter</button>
+  </div>
 </template>
-
 <script>
 import FullCalendar from '@fullcalendar/vue3';
 import dayGridPlugin from '@fullcalendar/daygrid';
@@ -13,34 +14,46 @@ import interactionPlugin from '@fullcalendar/interaction';
 
 export default {
   components: {
-    FullCalendar // make the <FullCalendar> tag available
+    FullCalendar 
   },
-  data: function() {
+  data() {
     return {
       calendarOptions: {
         plugins: [dayGridPlugin, interactionPlugin],
         initialView: 'dayGridMonth',
         weekends: true,
+        select: this.handleDateSelect,
         headerToolbar: {
           left: '',
           center: 'title',
           right: 'prev,next',
         },
         selectable: true,
-      }
+      },
+      selectedDate: null,
+      date: new Date().toLocaleDateString(),
     }
   },
-  mounted() {
-    this.calendar = new Calendar(this.$el, this.calendarOptions);
-    this.calendar.render();
-  },
   methods: {
-    handleDateClick(info) {
-      const dateStr = info.dateStr;
-      const dataDate = info.el.getAttribute('data-date');
-      console.log('Clicked on: ' + dataDate);
-      this.$router.push({ name: 'HomePage', query: { date: dateStr } });
+    handleDateSelect(info) {
+      let dateSelected = info.startStr
+      this.selectedDate = dateSelected
+      // console.log(this.selectedDate)
+    },
+    logSelectedDate() {
+      this.date = this.selectedDate;
+      console.log(this.date);
     }
   }
 }
-</script>
+</script> 
+
+<style>
+  button {
+    background-color: rgb(126, 216, 87); /* change the background color of the button to lime green */
+    border-radius: 10px; /* make the edges of the button rounded */
+    border: 2px solid rgb(126, 216, 87); /* add a lime green border to the button */
+    padding: 10px 50px; /* add some padding to the button */
+    color: white;
+  }
+</style>
--- src/views/CalendarPage.vue
+++ src/views/CalendarPage.vue
@@ -1,57 +1,56 @@
 <template> 
-    <div>
-      <NavigationBar/>
-      <div class="calendar-wrapper">
-        <FullCalendar class="calendar-container" :dateClick="handleDateClick"/>
-      </div>
-      <!-- <full-calendar :plugins="calendarPlugins" :initialView="'dayGridMonth'"/> -->
+  <div>
+    <NavigationBar/>
+    <div class="calendar-wrapper">
+      <FullCalendar class="calendar-container" :dateClick="handleDateClick"/>
     </div>
-  </template>
-    
-  <script>
-  import NavigationBar from "@/components/NavigationBar.vue";
-  import FullCalendar from "@/components/FullCalendar.vue";
+  </div>
+</template>
+  
+<script>
+import NavigationBar from "@/components/NavigationBar.vue";
+import FullCalendar from "@/components/FullCalendar.vue";
 //   import dayGridPlugin from "@fullcalendar/daygrid";
 //   import "@fullcalendar/common/main.css";
 //   import "@fullcalendar/daygrid/main.css";
-  
-  export default {
-    name: "CalendarPage",
-    components: {
-      NavigationBar,
-      FullCalendar,
-    },
-    methods: {
-        handleDateClick(info) {
-        // info.date is the clicked date
-        this.$router.push({ path: "/HomePage" }); 
-        }
-    },
-    data() {
-      return {
-        // calendarPlugins: [dayGridPlugin],
-      };
-    },
-  };
-  </script>
-  
+
+export default {
+  name: "CalendarPage",
+  components: {
+    NavigationBar,
+    FullCalendar,
+  },
+  methods: {
+      handleDateClick(info) {
+      // info.date is the clicked date
+      this.$router.push({ path: "/HomePage" }); 
+      }
+  },
+  data() {
+    return {
+      // calendarPlugins: [dayGridPlugin],
+    };
+  },
+};
+</script>
+
 <style>
 .calendar-wrapper {
-  display: flex;
-  justify-content: center;
-  margin: 20px auto 0 auto; /* Add 20px margin to the top */
-  width: 100%;
+display: flex;
+justify-content: center;
+margin: 40px auto 0 auto;
+width: 100%;
 }
 
 .calendar-container {
-  width: 60%;
-  height: 50%;
-  background-color: white;
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+width: 60%;
+height: 50%;
+background-color: white;
+font-family: Avenir, Helvetica, Arial, sans-serif;
 }
 
 .fc-daygrid-day-number, .fc-col-header-cell-cushion {
-    color: rgb(19, 3, 53);
-    text-decoration: none;
+  color: rgb(19, 3, 53);
+  text-decoration: none;
 }
 </style>
