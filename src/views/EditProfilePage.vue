71c75d1c93a1ce1d5d20d005cb22bd5054457312
kaitlyngoh <e0774226@u.nus.edu>
2023-03-29 01:20:40 +0800

    profile page ui done

---
+++ src/views/EditProfilePage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <NavigationBar/>
+      <h1>THIS IS EDIT PROFILE PAGE</h1>
+    </div>
+</template>
+  
+<script>
+  import NavigationBar from "@/components/NavigationBar.vue"
+  
+  export default {
+      name:"EditProfilePage" ,
+      components : {
+          NavigationBar,
+      },
+  }
+</script>
