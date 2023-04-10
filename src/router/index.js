1f86036e373f1aee199eedc9103b620a6b0b6d55
summerthia <summerthia12@gmail.com>
2023-04-10 14:35:50 +0800

    profile page and edit profile kait and mx

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,7 @@ import AddExerPage from '@/views/AddExerPage.vue'
 import CalendarPage from '@/views/CalendarPage.vue'
 import QuickAddCustom from '@/views/QuickAddCustom.vue'
 import EditFoodPage from '@/views/EditFoodPage.vue'
+import EditProfilePage from '@/views/EditProfilePage.vue'
 import { nextTick } from 'vue';
 
 
@@ -76,6 +77,12 @@ const routes = [
     path: '/QuestionnairePage',
     name: 'QuestionnairePage',
     component: QuestionnairePage
+  },
+
+  {
+    path: '/EditProfilePage',
+    name: 'EditProfilePage',
+    component: EditProfilePage
   },
 
   {
--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -4,36 +4,40 @@
     <div class="picandbutton">
       <div class="profilepic">
       <!-- rng some pic later -->
-        <span class="pic" style="font-size: 20px; font-weight: 800;">pic here</span>
+        <img class="pic" src="@/assets/images/Profile/tempDP.png" alt="profile picture" style="width: 100px; height: 120px">
       </div>
       <button v-on:click="goToEditProfilePage" class="editprofile">Edit Profile</button>
     </div>
   
     <div class="details">
-      <span class="username" style="font-size: 28px; font-weight: 500;">fishfanbloop</span>
+      <div class="usernamegender">
+        <span class="username" style="font-size: 28px; font-weight: 500;">{{ this.username }}</span>
+        <img v-if="this.gender === 'Female'" class="gender" src="@/assets/images/Profile/Female.png" alt="Female" style="width: 30px; height: 40px">
+        <img v-else class="gender" src="@/assets/images/Profile/Male.png" alt="Male" style="width: 30px; height: 40px">
+      </div>
         <div class="heightweight">
           <div class="hwdeets">
             <span class="height">
               <span class="heightnum" style="font-size: 20px; font-weight: 800;">Height</span>
-              <span style="font-size: 20px; font-weight: 700; color:  grey;">165cm</span>
+              <span style="font-size: 20px; font-weight: 700; color:  grey;">{{ this.height }} cm</span>
             </span>
             <span class="weight">
               <span class="weightnum" style="font-size: 20px; font-weight: 800;">Weight</span>
-              <span style="font-size: 20px; font-weight: 700; color:  grey;">50kg</span>
+              <span style="font-size: 20px; font-weight: 700; color:  grey;">{{ this.weight }} kg</span>
             </span>
           </div>
         </div>
         <div class="activity">
           <h4 style="font-size: 20px; font-weight: 800;">Daily Intake Goal</h4>
-          <h4 style="font-size: 20px; font-weight: 700; color:  grey;">2000 Calories</h4>
+          <h4 style="font-size: 20px; font-weight: 700; color:  grey;">{{ this.calories}} Calories</h4>
           <h4 style="font-size: 20px; font-weight: 800;">Activity Level</h4>
-          <h4 style="font-size: 20px; font-weight: 700; color:  grey;">Moderate</h4>
+          <h4 style="font-size: 20px; font-weight: 700; color:  grey;">{{ this.activity }}</h4>
         </div>
         <div class="program">
           <h4 style="font-size: 20px; font-weight: 800;">Program</h4>
-          <h4 style="font-size: 20px; font-weight: 700; color:  grey;">Gradual Weight Loss</h4>
+          <h4 style="font-size: 20px; font-weight: 700; color:  grey;">{{ this.program }}</h4>
           <h4 style="font-size: 20px; font-weight: 800;">Start Date</h4>
-          <h4 style="font-size: 20px; font-weight: 700; color: grey;"> 20 Jul 2022</h4>
+          <h4 style="font-size: 20px; font-weight: 700; color: grey;">{{ this.startdate }}</h4>
         </div>
     </div>
   </div>
@@ -41,13 +45,61 @@
 
 <script>
 import NavigationBar from "@/components/NavigationBar.vue"
+import { getAuth, onAuthStateChanged } from "@firebase/auth";
+import { getFirestore, collection, getDoc, getDocs, query, where, doc} from 'firebase/firestore';
 
 export default {
     name:"ProfilePage" ,
     components : {
         NavigationBar,
     },
+    data() {
+      return {
+        username: "",
+        gender: "",
+        height:0,
+        weight:0,
+        calories: 0,
+        activity: "xxx",
+        program: "xxx",
+        startdate: "xx"
+      }
+    },
+    created() {
+      this.renderProfile();
+    },
     methods : {
+      async renderProfile() {
+      return new Promise(async (resole, reject) => {
+        const auth = getAuth();
+        onAuthStateChanged(auth, async (user) => {
+          if (user) {
+            this.useremail = user.email;
+            // steps to retrieve from questionnaire
+            const userCollection = collection(getFirestore(), "Users");
+            const profileQuery = query(
+              userCollection,
+              where("email", "==", this.useremail),
+            );
+            const querySnapshot = await getDocs(profileQuery);
+            const userDocument = (querySnapshot.docs)[0]
+            this.username = userDocument.data().userName;
+            this.gender = userDocument.data().gender;
+            this.height = userDocument.data().height;
+            this.weight = userDocument.data().weight;
+            this.calories = userDocument.data().dailyIntakeGoal;
+            this.activity = userDocument.data().exercise;
+            this.program = userDocument.data().goal;
+            this.startdate = userDocument.data().startDate;
+            //userDocument.data().dailyIntakeGoal
+            resolve(0);
+          } else {
+            reject("User not authenticated.");
+          }
+        }
+        )
+      })
+    },
       goToEditProfilePage() {
         this.$router.push("/EditProfilePage");
       }
@@ -89,6 +141,15 @@ export default {
   display: inline-block;
   flex-direction: column;
   /* margin-top: 200px; */
+}
+
+.usernamegender {
+  display: flex;
+}
+
+.gender {
+  flex-grow: 0;
+  margin-left: 2vw;
 }
 
 .username {
